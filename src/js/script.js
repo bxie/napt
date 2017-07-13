6cd7d81d7000d475f2d793514f89402bb96b8a90
Hieke Keuning <hw.keuning@windesheim.nl>
2017-07-12 21:21:13 -0400

    edits

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,30 @@
-editor = ace.edit("editor");
-// editor.setTheme("ace/theme/monokai");
-editor.getSession().setMode("ace/mode/java");
-document.getElementById('editor').style.fontSize='14px';
+$(document).ready(function () 
+{
+    
+    $("#showtab2").click(function () 
+        { 
+            $("#tab2").show();
+            $("#tab2").addClass('active');
+            $("#tab1").removeClass('active');
+        }
+    );
 
-highlight(3);
+    $("#showtab3").click(function () 
+        { 
+            $("#tab3").show(); 
+        }
+    );
+
+    editor = ace.edit("editor");
+    //editor.setTheme("ace/theme/monokai");
+    editor.getSession().setMode("ace/mode/java");
+    document.getElementById('editor').style.fontSize = '14px';
+    editor.setReadOnly(true);
+
+    highlight(3);
+   
+});
+
 
 function highlight(line)
 {
@@ -16,7 +37,3 @@ function gotoNext(line)
     editor.gotoLine(editor.getCursor() + 1, 0, true);
 }
 
-function showNextTab()
-{
-
-}
